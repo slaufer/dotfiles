3b2d500eac0204dcfd12b52a4dc9132a023dda54
Scott Laufer <slaufer@gmail.com>
2017-11-11 00:10:08 -0500

    More cleanups on vexing

--- home/.bash/prompt/vexing.sh
+++ home/.bash/prompt/vexing.sh
@@ -1,46 +1,47 @@
 #!/bin/bash
+# VEXING requires 256 color support
+if (( $(tput colors) < 256 )); then
+	echo VEXING requires 256 color support
+	return
+fi
 
 #################
 # CONFIGURATION #
 #################
 
+# TODO: make this dynamic?
 _VEXING_DIR_WIDTH=70
+
+# Prompt colors
+# These color codes have to be properly enclosed to prevent wrapping issues
+_VEXING_COLORS=(
+	'\[\e[0m\]'                  # 0: reset
+	"\[\e[38;5;25m\e[48;5;0m\]"  # 1: Brackets, dark blue
+	"\[\e[38;5;35m\e[48;5;0m\]"  # 2: Git clean/staged, sea green
+	"\[\e[38;5;39m\e[48;5;0m\]"  # 3: Clock and user@host foreground, dark cyan
+	"\[\e[38;5;44m\e[48;5;0m\]"  # 4: Directory foreground, bright cyan
+	"\[\e[38;5;45m\e[48;5;0m\]"  # 5: Prompt ($/#), sky blue
+	"\[\e[38;5;69m\e[48;5;0m\]"  # 6: Clock, directory and user@host background, denim blue
+	"\[\e[38;5;160m\e[48;5;0m\]" # 7: Git untracked, red
+	"\[\e[38;5;172m\e[48;5;0m\]" # 8: Git unstaged, burnt orange
+	"\[\e[38;5;252m\e[48;5;0m\]" # 9: Command count, light gray
+)
 
 ####################
 # Helper Functions #
 ####################
 
-# This function sets ANSI colors.
-# Usage:
-# - col r
-#     reset colors
-# - col 3 <0-7> <0-7>
-#     set 3-bit foreground and background color; value range is 0-7
-# - col 8 <fg> <bg>
-#     set 8-bit foreground and background color; value range is 0-255
-# - col 24 <fg red> <fg green> <fg blue> <bg red> <bg green> <bg blue>
-#     set 24-bit background color; value range is 0-255
-
-_VEXING_COLORS=(
-	'\[\e[0m\]'                  # 0: reset
-	"\[\e[38;5;25m\e[48;5;0m\]"  # 1: dark blue
-	"\[\e[38;5;35m\e[48;5;0m\]"  # 2: sea green
-	"\[\e[38;5;39m\e[48;5;0m\]"  # 3: bright green
-	"\[\e[38;5;44m\e[48;5;0m\]"  # 4: bright cyan
-	"\[\e[38;5;45m\e[48;5;0m\]"  # 5: sky blue
-	"\[\e[38;5;69m\e[48;5;0m\]"  # 6: light purple-blue
-	"\[\e[38;5;160m\e[48;5;0m\]" # 7: dark red
-	"\[\e[38;5;172m\e[48;5;0m\]" # 8: burnt orange
-	"\[\e[38;5;252m\e[48;5;0m\]" # 9: light gray
-)
-
+# Colorizes a path, and replaces home with ~
 function _VEXING_color_dir {
-	# convert $HOME to ~
-	local TILDE='~'
-	local DIR=${1/$HOME/$TILDE}
-	DIR=$(echo $DIR | tail -c $_VEXING_DIR_WIDTH)
-	DIR=${DIR//\//${_VEXING_COLORS[6]}/${_VEXING_COLORS[4]}}
-	echo -n $DIR
+	# get directory with homedir substitution
+	local dir=$(dirs +0)
+
+	# trim directory
+	(( ${#dir} > $_VEXING_DIR_WIDTH )) && dir=${dir: -$_VEXING_DIR_WIDTH}
+
+	# color directory
+	dir=${dir//\//${_VEXING_COLORS[6]}/${_VEXING_COLORS[4]}}
+	echo -n $dir
 }
 
 ####################
@@ -50,35 +51,37 @@ function _VEXING_color_dir {
 # 256-color prompt for xterm-like terminals
 function _VEXING_prompt_8bit {
 	local EXIT=$?
-	local PROMPT=
+	local PROMPT
 	
 	# status line clock
-	PROMPT+="${_VEXING_COLORS[1]}[${_VEXING_COLORS[3]}$(date +%l)${_VEXING_COLORS[6]}:${_VEXING_COLORS[3]}$(date +%M%P)${_VEXING_COLORS[1]}]"
+	local TIME=($(date '+%l %M%P'))
+	PROMPT+="${_VEXING_COLORS[1]}[${_VEXING_COLORS[3]}${TIME[0]}${_VEXING_COLORS[6]}:${_VEXING_COLORS[3]}${TIME[1]}${_VEXING_COLORS[1]}]"
 	# status line working directory
 	PROMPT+=" ${_VEXING_COLORS[1]}[${_VEXING_COLORS[4]}$(_VEXING_color_dir "$PWD")${_VEXING_COLORS[1]}]"
 
 
 	# git status
-	if [[ -z "$_VEXING_NOGIT" ]]; then
+	local GIT_STATUS GIT_EXIT=1 # because "local" has a return code
+
+	if [[ -z $_VEXING_NOGIT ]]; then
 		# either a status (possibly empty) or the return code
-		local GITSTATUS=$(git status -s 2> /dev/null || echo FAIL)
-	else
-		# just set it to the failure return code
-		local GITSTATUS=FAIL
+		GIT_STATUS=$(git status -s 2> /dev/null)
+		GIT_EXIT=$?
 	fi
 
-	if [[ "$GITSTATUS" != FAIL ]]; then
+	if (( $GIT_EXIT == 0 )); then
 		# figure out status counts
-		local UNTRACKED=$(grep '^[ ?][ ?]' -c <<< "$GITSTATUS")
-		local UNSTAGED=$(grep '^ [MADRC]' -c <<< "$GITSTATUS")
-		local STAGED=$(grep '^[MADRC]' -c <<< "$GITSTATUS")
+		local UNTRACKED=$(grep '^[ ?][ ?]' -c <<< "$GIT_STATUS")
+		local UNSTAGED=$(grep '^ [MADRC]' -c <<< "$GIT_STATUS")
+		local STAGED=$(grep '^[MADRC]' -c <<< "$GIT_STATUS")
 
+		# figure out how to color the branch name
 		local BRANCH_COLOR=${_VEXING_COLORS[2]}
 		(( $UNSTAGED > 0 )) || (( $UNTRACKED > 0 )) && BRANCH_COLOR=${_VEXING_COLORS[7]}
 		(( $STAGED > 0 ))  && BRANCH_COLOR=${_VEXING_COLORS[8]} 
 		PROMPT+=" ${_VEXING_COLORS[1]}[${BRANCH_COLOR}$(git rev-parse --abbrev-ref HEAD)${_VEXING_colors[0]}"
 
-		[[ $UNTRACKED -gt 0 ]] &&	PROMPT+="${_VEXING_COLORS[7]} ${UNTRACKED}" 
+		[[ $UNTRACKED -gt 0 ]] && PROMPT+="${_VEXING_COLORS[7]} ${UNTRACKED}" 
 		[[ $UNSTAGED -gt 0 ]] && PROMPT+="${_VEXING_COLORS[8]} ${UNSTAGED}"
 		[[ $STAGED -gt 0 ]] && PROMPT+="${_VEXING_COLORS[2]} ${STAGED}"
 
@@ -94,16 +97,11 @@ function _VEXING_prompt_8bit {
 	PROMPT+="\n${_VEXING_COLORS[1]}[${_VEXING_COLORS[9]}#\#${_VEXING_colors[0]}${_VEXING_COLORS[1]}] " # command number
 
 	# user, host and prompt
-	PROMPT+="${_VEXING_COLORS[1]}${_VEXING_COLORS[3]}\u${_VEXING_COLORS[6]}@${_VEXING_COLORS[3]}\h${_VEXING_COLORS[1]} ${_VEXING_COLORS[5]}\$ ${_VEXING_COLORS[0]}"
+	PROMPT+="${_VEXING_COLORS[3]}\u${_VEXING_COLORS[6]}@${_VEXING_COLORS[3]}\h ${_VEXING_COLORS[1]}${_VEXING_COLORS[5]}\$ ${_VEXING_COLORS[0]}"
 
 	# interpret color codes and set the prompt	
 	PS1=$(echo -e $PROMPT)
 }
 
-# only activate the fancy prompt if this is a 256-color terminal
-if (( $(tput colors) >= 256 )) ; then
-	PROMPT_COMMAND=_VEXING_prompt_8bit
-else
-	echo 'VEXING: 256 color mode unavailable, using mono fallback prompt'
-	PS1='($?) \u@\h \w \$ '
-fi
+PROMPT_COMMAND=_VEXING_prompt_8bit
+
