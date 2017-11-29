e3ec596a30f5a75b3edb4371f31e9ec6ce4c26b2
Scott Laufer <slaufer@synacor.com>
2017-11-28 20:46:18 -0500

    Rewrote vboxmons.sh

--- home/bin/vboxmons.sh
+++ home/bin/vboxmons.sh
@@ -1,28 +1,52 @@
 #!/bin/bash
-set -x
-XRANDR_OPTS="--verbose"
-MONPREFIX="VGA-"
 
-# if the monitor count is less than 1, exit
-[ $1 -lt 1 ] && exit 1
-
-# disable all monitors except the first one, because disabling all screens makes X crash
-xrandr | awk '
-/^'$MONPREFIX'[0-9]+/ {
-	if (match($1, "^'$MONPREFIX'0+$")) {
-		next
-	}
-
-	print("++ xrandr '$XRANDR_OPTS' --output " $1 " --off\n")
-	system("xrandr '$XRANDR_OPTS' --output " $1 " --off")
+# prints help message
+function printhelp {
+	echo "vboxmons.sh: Enables a given number of monitors (usually in VirtualBox)" >&2
+	echo "USAGE: $0 <count>" >&2
+	[[ ! -z $1 ]] && echo Error: $1 >&2
+	exit 1
 }
-'
 
-# set up the first monitor with absolute position
-xrandr $XRANDR_OPTS --output ${MONPREFIX}0 --auto
+# get argument
+count=$1
 
-# set up the rest relative to the first
-for i in `seq 1 $(($1 - 1))`; do
-	xrandr $XRANDR_OPTS --output ${MONPREFIX}${i} --auto --left-of ${MONPREFIX}$(($i - 1))
+# make sure there is an argument
+[[ -z $count ]] && printhelp "no count given"
+
+# make sure the argument is numeric
+[[ ! $count =~ ^[0-9]+$ ]] && printhelp "argument must be numeric"
+
+# make sure the argument is valid
+(( count < 1 )) && printhelp "argument must be > 1"
+
+# get monitor list
+mons=$(xrandr | grep connected | cut -d' ' -f1 | sort)
+mons=(VGA-0 VGA-1 VGA-2 VGA-3 VGA-4)
+echo "Found ${#mons} monitors: ${mons[@]}"
+
+# disable all but the first monitor -- disabling the first one crashes the X server
+for mon in ${mons[@]:1}; do
+	echo Running: xrandr --output $mon --off
+	xrandr --output $mon --off
 done
 
+# enable however many monitors we wanted
+i=0
+for mon in ${mons[@]}; do
+	# if we've enabled enough monitors, stop here
+	(( i == count )) && break
+
+	# if this isn't the first monitor, set it to the right of the previous one
+	if [[ -z $lastmon ]]; then
+		echo Running: echo xrandr --output $mon --auto
+		xrandr --output $mon --auto
+	else
+		echo Running: xrandr --output $mon --auto --right-of $lastmon
+		xrandr --output $mon --auto --right-of $lastmon
+	fi	
+
+	# remember the previous monitor
+	((i++))
+	lastmon=$mon
+done
