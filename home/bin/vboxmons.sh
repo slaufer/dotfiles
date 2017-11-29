154a9052d0a7e6ac6a7339e615e6c143048c6aac
Scott Laufer <slaufer@synacor.com>
2017-11-28 21:10:14 -0500

    Added some comments to vboxmons.sh

--- home/bin/vboxmons.sh
+++ home/bin/vboxmons.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+# vboxmons.sh - Enables/disables monitors in sequence
+#
+# The purpose of this script is to quickly enable and disable extra monitors in VirtualBox when I take my work laptop
+# out of the dock.
+#
+# Usage: vboxmons.sh <# of monitors to enable>
 
 # prints help message
 function printhelp {
 	echo "vboxmons.sh: Enables a given number of monitors (usually in VirtualBox)" >&2
-	echo "USAGE: $0 <count>" >&2
+	echo "USAGE: $0 <# of monitors to enable>" >&2
 	[[ ! -z $1 ]] && echo Error: $1 >&2
 	exit 1
 }
@@ -35,17 +41,19 @@ i=0
 for mon in ${mons[@]}; do
 	# if we've enabled enough monitors, stop here
 	(( i == count )) && break
+	((i++))
 
-	# if this isn't the first monitor, set it to the right of the previous one
+	# enable monitor
 	if [[ -z $lastmon ]]; then
+		# if this is the first monitor, don't include layout argument
 		echo Running: xrandr --output $mon --auto
 		xrandr --output $mon --auto
 	else
+		# if this isn't the first monitor, set it to the right of the previous one
 		echo Running: xrandr --output $mon --auto --right-of $lastmon
 		xrandr --output $mon --auto --right-of $lastmon
 	fi	
 
 	# remember the previous monitor
-	((i++))
 	lastmon=$mon
 done
